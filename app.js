e8ddd25a9beadf5d4df2770f11492ba849a71cb4
hy2n <25565k@daum.net>
2024-04-14 08:09:18 +0900

    Update 9

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-console.log("Hello World")
-//Print Hello world.
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000; // 사용할 포트 번호
+
+// 정적 파일 제공
+app.use(express.static('public'));
+
+// 루트 경로 요청 시 index.html 파일 제공
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// 서버 시작
+app.listen(port, () => {
+  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다!`);
+});
