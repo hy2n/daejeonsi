2688d20e6c2d97058c1e1143d1f3bb1e932f6b6a
hy2n <25565k@daum.net>
2024-05-04 17:25:02 +0900

    ROUTER UPDATE

--- routes/join.js
+++ routes/join.js
@@ -1,10 +1,28 @@
 const express = require('express');
+var bodyParser = require('body-parser')
 const router = express.Router();
+const path = require('path'); // path 모듈 import
+const fs = require('fs'); // fs 모듈 import
+
+router.use(express.urlencoded( {extended : false } ));
+router.use(bodyParser.json());
+
+// 유저 정보 저장한 파일 불러오기(실제로는 DB연결)
+const usersFilePath = path.join(__dirname,  './../data/db/users.json');
+
+// 회원 정보를 받아오기
+let users = [];
+try {
+  users = JSON.parse(fs.readFileSync(usersFilePath, 'utf8'));
+} catch (err) {
+  console.error('사용자 정보를 읽어오는 데 문제가 발생했습니다.', err);
+}
+
 
 router.post('/', (req, res) => {
-    const { id, password } = req.body;
+    const { id, password, stdudentid } = req.body;
   
-    if (!id || !password) {
+    if (!id || !password || !stdudentid) {
       return res.status(400).json({ error: 'ID와 비밀번호를 모두 입력해주세요.' });
     }
   
