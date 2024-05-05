4df82d9458856f8118748cf984ca760414d946bb
hy2n <25565k@daum.net>
2024-05-05 22:43:29 +0900

    👌보안 알고리즘 적용 (2)

--- routes/login.js
+++ routes/login.js
@@ -22,30 +22,41 @@ const usersFilePath = path.join(__dirname, './../data/db/users.json');
 // 로그인 엔드포인트
 router.post('/', (req, res) => {
   // 사용 예시
-  security.CheckVoid(req.headers['x-forwarded-for'] || requestIp.getClientIp(req));
-  // 회원 정보를 받아오기
-  let users = [];
-  try {
-    users = JSON.parse(fs.readFileSync(usersFilePath, 'utf8'));
-  } catch (err) {
-    console.error('사용자 정보를 읽어오는 데 문제가 발생했습니다.', err);
-  }
+  security.isBlocked(req.headers['x-forwarded-for'] || requestIp.getClientIp(req), result => {
+    if (result) {
+      console.log("[알림] 사용자 제한됨");
+      return res.send('<script>alert("보안상 일시적으로 접근할 수 없습니다. 잠시 후 다시 시도하세요.");window.history.back();</script>');
+    }
+    else {
+      // 회원 정보를 받아오기
+      let users = [];
+      try {
+        users = JSON.parse(fs.readFileSync(usersFilePath, 'utf8'));
+      } catch (err) {
+        console.error('사용자 정보를 읽어오는 데 문제가 발생했습니다.', err);
+      }
 
-  const { id, password, } = req.body;
-  const user = users.find(user => user.id === id && user.password === hashPassword(password));
-  if (!user) {
-    return res.send('<script>alert("아이디나 비빌번호가 일치하지 않습니다. 잔여 시도 횟수 : ' + 1 + '회");window.history.back();</script>');
-  }
+      const { id, password, } = req.body;
+      const user = users.find(user => user.id === id && user.password === hashPassword(password));
+      if (!user) {
+        security.UserTriggered(req.headers['x-forwarded-for'] || requestIp.getClientIp(req)), (result) => {
+          if (result) return res.send('<script>alert("보안상 일시적으로 접근할 수 없습니다. 잠시 후 다시 시도하세요.");window.history.back();</script>');
+        };
+        console.log("[알림] 사용자 DB접근");
+        return res.send('<script>alert("아이디나 비빌번호가 일치하지 않습니다. 계속해서 일치하지 않으면 제한됩니다.");window.history.back();</script>');
+      }
 
-  const studentid = user.studentid;
-  const name = user.name;
+      const studentid = user.studentid;
+      const name = user.name;
 
-  // JWT 토큰 생성
-  const token = jwt.sign({ id, studentid,name }, 'secret_key', { expiresIn: '1y' });
+      // JWT 토큰 생성
+      const token = jwt.sign({ id, studentid, name }, 'secret_key', { expiresIn: '1y' });
 
-  // 쿠키에 토큰 저장
-  res.cookie('token', token, { httpOnly: true });
-  res.send('<meta http-equiv="refresh" content="0; url=/home"></meta><script>alert("로그인 완료!");</script>');
+      // 쿠키에 토큰 저장
+      res.cookie('token', token, { httpOnly: true });
+      res.send('<meta http-equiv="refresh" content="0; url=/home"></meta><script>alert("로그인 완료!");</script>');
+    }
+  });
 });
 
 function hashPassword(password) {
